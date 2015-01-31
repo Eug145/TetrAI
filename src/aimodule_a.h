a545af107f92230a3cf5a29b068c16c028eab6b6
Eugene Zavidovsky <mumu3920@yahoo.com>
2015-01-31 12:19:17 +0200

    AIModule v0.06

--- src/aimodule_a.h
+++ src/aimodule_a.h
@@ -18,7 +18,7 @@
  *  You should have received a copy of the GNU Lesser General Public License 
  *  along with this program. If not, see: <http://www.gnu.org/licenses/>.
  */
-
+  
 #ifndef AIMODULE_A_H
 #define AIMODULE_A_H
 
--- src/aimodule_b_intf_aa.h
+++ src/aimodule_b_intf_aa.h
@@ -18,7 +18,7 @@
  *  You should have received a copy of the GNU Lesser General Public License 
  *  along with this program. If not, see: <http://www.gnu.org/licenses/>.
  */
- 
+   
 #ifndef AIMODULE_B_INTF_AA_H
 #define AIMODULE_B_INTF_AA_H
 
